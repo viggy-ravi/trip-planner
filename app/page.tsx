4af152829df1dfff443b58c69ca064cc15aceb7e
Vignesh Ravindranath <vignesh.ravindranath@gmail.com>
2026-08-28 16:32:14 -0500

    Convert trips to React state

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,15 @@
+"use client";
+
 import { Trip } from "./types";
 import TripListItem from "./components/TripListItem";
-
-const trips: Trip[] = [
-  { id: 1, name: "Summer in Japan", destination: "Tokyo, Japan", startDate: "2026-09-10" },
-  { id: 2, name: "Ski Trip", destination: "Aspen, Colorado", startDate: "2027-01-05" },
-];
+import { useState } from "react";
 
 export default function Home() {
+  const [trips, setTrips] = useState<Trip[]>([
+    { id: 1, name: "Summer in Japan", destination: "Tokyo, Japan", startDate: "2026-09-10" },
+    { id: 2, name: "Ski Trip", destination: "Aspen, Colorado", startDate: "2027-01-05" },
+  ]);
+
   return (
     <div>
       <h1>My Trips</h1>
